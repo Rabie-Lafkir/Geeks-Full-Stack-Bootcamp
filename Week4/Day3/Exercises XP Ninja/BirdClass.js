8869fff212dc75054877c40294faaccc5e16e10e
RABIE LAFKIR <lafkirrabie2000@gmail.com>
2025-07-03 12:00:51 +0100

    Week4 - Day3 - Ninja - Completed

---
+++ Week4/Day3/Exercises XP Ninja/BirdClass.js
@@ -0,0 +1,14 @@
+class Bird {
+  constructor() {
+    console.log("I'm a bird. 🦢");
+  }
+}
+
+class Flamingo extends Bird {
+  constructor() {
+    console.log("I'm pink. 🌸");
+    super();
+  }
+}
+
+const pet = new Flamingo(); // logs: "I'm pink. 🌸" then "I'm a bird. 🦢"
