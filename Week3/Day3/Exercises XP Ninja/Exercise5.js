2a9d9d15d9beacd5e6a5b6555eb0345afb29e4c1
RABIE LAFKIR <lafkirrabie2000@gmail.com>
2025-07-11 08:30:40 +0100

    Code review

--- Week3/Day3/Exercises XP Ninja/Exercise5.js
+++ Week3/Day3/Exercises XP Ninja/Exercise5.js
@@ -4,7 +4,24 @@
  * @returns 
  */
 function uniqueElements(array) {
-    return [...new Set(array)];
+    let unique = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        let isDuplicate = false;
+        
+        for (let j = 0; j < unique.length; j++) {
+            if (array[i] === unique[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        
+        if (!isDuplicate) {
+            unique.push(array[i]);
+        }
+    }
+    
+    return unique;
 }
 
 console.log(uniqueElements([1,2,3,3,3,4,5]));
