c7949445128fffa6682b4ee54da757fd103cb2d4
RABIE LAFKIR <lafkirrabie2000@gmail.com>
2025-07-11 11:04:29 +0100

    Redo creation

--- Week6/Day3/Mini Projects/chat-app/src/config/db.js
+++
@@ -1,9 +0,0 @@
-import knex from 'knex';
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from './env.js';
-
-export const db = knex({
-    client: 'pg',
-    connection: { host: DB_HOST, user: DB_USER, password: DB_PASSWORD, database: DB_NAME },
-    migrations: { directory: 'migrations' },
-    seeds: { directory: 'seeds' }
-});
